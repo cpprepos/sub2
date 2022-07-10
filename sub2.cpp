21c698e95c7f57b56474154c28a3f9df0d2f5589
crissu <13750782397@163.com>
2022-07-10 07:10:37 +0000

    add

---
+++ sub2.cpp
@@ -0,0 +1,8 @@
+//sub1.cpp
+#include"sub1.h"
+#include<iostream>
+
+void printSub1()
+{
+    std::cout<<"hello world!!!\n";
+}
---
+++ sub2.h
@@ -0,0 +1,8 @@
+//sub2.h
+#ifndef _SUB2_h_
+#define _SUB2_H_
+
+#include<iostream>
+void printSub2();
+
+#endif
